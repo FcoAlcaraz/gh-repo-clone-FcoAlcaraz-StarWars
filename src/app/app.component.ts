24f53bece475dfeafad794539c2e10144f449f94
FcoAlcaraz <84114614+FcoAlcaraz@users.noreply.github.com>
2023-04-04 11:00:21 -0700

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
+  imports: [CommonModule, HomeComponent, AppComponent],
   standalone: true,
   selector: 'app-app',
   templateUrl: './app.component.html',
