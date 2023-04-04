2b32e21be3edd71ed5f51938c5f3cc0a02720db1
FcoAlcaraz <84114614+FcoAlcaraz@users.noreply.github.com>
2023-04-04 15:36:50 -0700

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CharacterDetailsComponent } from './components/pages/characters/character-details/character-details.component';
 import { CharactersComponent } from './components/pages/characters/characters.component';
+import { MovieDetailsComponent } from './components/pages/movies/movie-details/movie-details.component';
 
 import { MoviesListComponent } from './components/pages/movies/movies-list/movies-list.component';
 import { CharacterResolverService } from './shared/services/character-resolver.service';
