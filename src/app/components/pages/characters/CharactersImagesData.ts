8bb41ab91f1167c9eccec0178d9a6382f9fb0b11
FcoAlcaraz <84114614+FcoAlcaraz@users.noreply.github.com>
2023-04-05 19:47:11 -0700

    2 files modified

--- src/app/components/pages/characters/CharactersImagesData.ts
+++ src/app/components/pages/characters/CharactersImagesData.ts
@@ -1,22 +1,102 @@
 export const CharactersImage = [
-  { name: "Luke Skywalker", url: "https://swapi.dev/api/people/1/", src: "/assets/imgs/characters/LukeSkywalker.jpg" },
-  { name: "C-3PO", url: "https://swapi.dev/api/people/2/", src: "/assets/imgs/characters/C-3PO.jpeg" },
-  { name: "R2-D2", url: "https://swapi.dev/api/people/3/", src: "/assets/imgs/characters/R2-D2.jpg" },
-  { name: "Darth Vader", url: "https://swapi.dev/api/people/4/", src: "/assets/imgs/characters/DarthVader.jpg" },
-  { name: "Leia Organa", url: "https://swapi.dev/api/people/5/", src: "/assets/imgs/characters/LeiaOrgana.jpg" },
-  { name: "Owen Lars", url: "https://swapi.dev/api/people/6/", src: "/assets/imgs/characters/OwenLars.jpg" },
-  { name: "Beru Whitesun lars", url: "https://swapi.dev/api/people/7/", src: "/assets/imgs/characters/BeruWhitesunlars.jpg" },
-  { name: "R5-D4", url: "https://swapi.dev/api/people/8/", src: "/assets/imgs/characters/R5-D4.jpg" },
-  { name: "Biggs Darklighter", url: "https://swapi.dev/api/people/9/", src: "/assets/imgs/characters/BiggsDarklighter.jpeg" },
-  { name: "Obi-Wan Kenobi", url: "https://swapi.dev/api/people/10/", src: "/assets/imgs/characters/Obi-WanKenobi.jpeg" },
-  { name: "Anakin Skywalker", url: "https://swapi.dev/api/people/11/", src: "/assets/imgs/characters/AnakinSkywalker.jpg" },
-  { name: "Wilhuff Tarkin", url: "https://swapi.dev/api/people/12/", src: "/assets/imgs/characters/WilhuffTarkin.jpg" },
-  { name: "Chewbacca", url: "https://swapi.dev/api/people/13/", src: "/assets/imgs/characters/Chewbacca.jpg" },
-  { name: "Han Solo", url: "https://swapi.dev/api/people/14/", src: "/assets/imgs/characters/HanSolo.jpg" },
-  { name: "Greedo", url: "https://swapi.dev/api/people/15/", src: "/assets/imgs/characters/Greedo.jpeg" },
-  { name: "Jabba Desilijic Tiure", url: "https://swapi.dev/api/people/16/", src: "/assets/imgs/characters/JabbaDesilijicTiure.jpg" },
-  { name: "Unknown", url: "https://swapi.dev/api/people/17/", src: "/assets/imgs/characters/unkown" },
-  { name: "Wedge Antilles", url: "https://swapi.dev/api/people/18/", src: "/assets/imgs/characters/WedgeAntilles.jpeg" },
-  { name: "Jek Tono Porkins", url: "https://swapi.dev/api/people/19/", src: "/assets/imgs/characters/JekTonoPorkins.jpgg" },
-  { name: "Yoda", url: "https://swapi.dev/api/people/20/", src: "/assets/imgs/characters/Yoda.jpeg" },
-]
+  {
+    name: 'Luke Skywalker',
+    url: 'https://swapi.dev/api/people/1/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/LukeSkywalker.jpg',
+  },
+  {
+    name: 'C-3PO',
+    url: 'https://swapi.dev/api/people/2/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/C-3PO.jpeg',
+  },
+  {
+    name: 'R2-D2',
+    url: 'https://swapi.dev/api/people/3/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/R2-D2.jpg',
+  },
+  {
+    name: 'Darth Vader',
+    url: 'https://swapi.dev/api/people/4/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/DarthVader.jpg',
+  },
+  {
+    name: 'Leia Organa',
+    url: 'https://swapi.dev/api/people/5/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/LeiaOrgana.jpg',
+  },
+  {
+    name: 'Owen Lars',
+    url: 'https://swapi.dev/api/people/6/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/OwenLars.jpg',
+  },
+  {
+    name: 'Beru Whitesun lars',
+    url: 'https://swapi.dev/api/people/7/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/BeruWhitesunlars.jpg',
+  },
+  {
+    name: 'R5-D4',
+    url: 'https://swapi.dev/api/people/8/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/R5-D4.jpg',
+  },
+  {
+    name: 'Biggs Darklighter',
+    url: 'https://swapi.dev/api/people/9/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/BiggsDarklighter.jpeg',
+  },
+  {
+    name: 'Obi-Wan Kenobi',
+    url: 'https://swapi.dev/api/people/10/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/Obi-WanKenobi.jpeg',
+  },
+  {
+    name: 'Anakin Skywalker',
+    url: 'https://swapi.dev/api/people/11/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/AnakinSkywalker.jpg',
+  },
+  {
+    name: 'Wilhuff Tarkin',
+    url: 'https://swapi.dev/api/people/12/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/WilhuffTarkin.jpg',
+  },
+  {
+    name: 'Chewbacca',
+    url: 'https://swapi.dev/api/people/13/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/Chewbacca.jpga',
+  },
+  {
+    name: 'Han Solo',
+    url: 'https://swapi.dev/api/people/14/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/HanSolo.jpg',
+  },
+  {
+    name: 'Greedo',
+    url: 'https://swapi.dev/api/people/15/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/Greedo.jpeg',
+  },
+  {
+    name: 'Jabba Desilijic Tiure',
+    url: 'https://swapi.dev/api/people/16/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/JabbaDesilijicTiure.jpg',
+  },
+  {
+    name: 'Unknown',
+    url: 'https://swapi.dev/api/people/17/',
+    src: '/assets/imgs/characters/unkown',
+  },
+  {
+    name: 'Wedge Antilles',
+    url: 'https://swapi.dev/api/people/18/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/WedgeAntilles.jpeg',
+  },
+  {
+    name: 'Jek Tono Porkins',
+    url: 'https://swapi.dev/api/people/19/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/JekTonoPorkins.jpg',
+  },
+  {
+    name: 'Yoda',
+    url: 'https://swapi.dev/api/people/20/',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/characters/Yoda.jpeg',
+  },
+];
--- src/app/components/pages/movies/MoviesImagesData.ts
+++ src/app/components/pages/movies/MoviesImagesData.ts
@@ -2,31 +2,31 @@ export const MovieImages = [
   {
     name: 'A New Hope',
     url: 'https://swapi.dev/api/films/1/',
-    src: '/assets/imgs/films/ANewHope.jpg',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/films/ANewHope.jpg',
   },
   {
     name: 'The Empire Strikes Back',
     url: 'https://swapi.dev/api/films/2/',
-    src: '/assets/imgs/films/TheEmpireStrikesBack.jpg',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/films/TheEmpireStrikesBack.jpg',
   },
   {
     name: 'Return of the Jedi',
     url: 'https://swapi.dev/api/films/3/',
-    src: '/assets/imgs/films/ReturnoftheJedi.jpg',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/films/ReturnoftheJedi.jpg',
   },
   {
     name: 'The Phantom Menace',
     url: 'https://swapi.dev/api/films/4/',
-    src: '/assets/imgs/films/ThePhantomMenace.jpg',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/films/ThePhantomMenace.jpg',
   },
   {
     name: 'Attack of the Clones',
     url: 'https://swapi.dev/api/films/5/',
-    src: '/assets/imgs/films/AttackoftheClones.jpg',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/films/AttackoftheClones.jpg',
   },
   {
     name: 'Revenge of the Sith',
     url: 'https://swapi.dev/api/films/6/',
-    src: '/assets/imgs/films/RevengeoftheSith.jpg',
+    src: 'https://cdn.jsdelivr.net/gh/FcoAlcaraz/gh-repo-clone-FcoAlcaraz-StarWars@main/src/app/assets/imgs/films/RevengeoftheSith.jpg',
   },
 ];
